62c5dac97cac719d8624df3cd1e70cabf11f9d65
hatanvir <tanvirhasan552@gmail.com>
2018-09-16 16:29:35 +0600

    MainActivity updated

--- app/src/main/java/com/example/tanvir/to_letapp/activity/MainActivity.java
+++ app/src/main/java/com/example/tanvir/to_letapp/activity/MainActivity.java
@@ -11,10 +11,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.tanvir.to_letapp.R;
+import com.example.tanvir.to_letapp.activity.ownerActivity.OwnerMainActivity;
+import com.example.tanvir.to_letapp.activity.renterActivity.RenterMainActivity;
 import com.example.tanvir.to_letapp.adapters.FlatAdapter;
 import com.example.tanvir.to_letapp.models.FlatDetails;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -30,8 +33,6 @@ public class MainActivity extends AppCompatActivity {
     FlatAdapter flatAdapter;
     FirebaseDatabase database;
     DatabaseReference databaseReference;
-    DatabaseReference databaseReferenceId;
-    String s;
 
     ArrayList<FlatDetails> arrayList = new ArrayList<>();
     @Override
@@ -40,28 +41,78 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Firebase.setAndroidContext(this);
+
         locationTv = findViewById(R.id.locationTv);
         availavbleForTv = findViewById(R.id.availableForTv);
         conditionTv = findViewById(R.id.flatConditionTv);
 
         listView = findViewById(R.id.listView);
 
-        // firebase = new Firebase("https://to-let-app-d0099.firebaseio.com/Owner/Post");
-        database = FirebaseDatabase.getInstance();
+        database = FirebaseDatabase.getInstance();//database refrence
         databaseReference = database.getReference().child("Owner").child("User");
 
-
         flatAdapter = new FlatAdapter(this, arrayList);
+        //when user on logged in then the user id will be return null
+        //thats why try catch given here
+        try{
+            String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();//current user id
+
+            DatabaseReference database = FirebaseDatabase.getInstance().getReference();//database reference
+
+                database.child("Owner").child("User").child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        //checking current user id exist or not
+                        //if exist then owner activity will be start
+                        if(dataSnapshot.exists()){
+                            Intent intent = new Intent(MainActivity.this, OwnerMainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            finish();
+                            startActivity(intent);
+                        }
+                        else{//else renter activity will be start
+                            Intent intent = new Intent(MainActivity.this, RenterMainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            finish();
+                            startActivity(intent);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+        }
+        catch (Exception e){
+            homeActivity();
+        }
+
+        //sending data to details activity
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(MainActivity.this,DetailsActivity.class);
+                intent.putExtra("location",arrayList.get(i).getOwnerId());
+                intent.putExtra("Condition",arrayList.get(i).getFlatCondition());
+                intent.putExtra("Available",arrayList.get(i).getAvailableFor());
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void homeActivity(){
 
         databaseReference.addValueEventListener(new com.google.firebase.database.ValueEventListener() {
             @Override
             public void onDataChange(@NonNull com.google.firebase.database.DataSnapshot dataSnapshot) {
                 for (com.google.firebase.database.DataSnapshot d : dataSnapshot.getChildren()) {
                     firebase = new Firebase("https://to-let-app-d0099.firebaseio.com/Owner/User/"+d.getKey()+"/Post");
-                    //databaseReferenceId = d.getRef().child(d.getKey()).child("Post");
-                    //Toast.makeText(MainActivity.this, ""+firebase.getKey(), Toast.LENGTH_SHORT).show();
+
+
                     final String id = d.getKey();
-                    Toast.makeText(MainActivity.this, ""+id, Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(MainActivity.this, ""+id, Toast.LENGTH_SHORT).show();
 
                     firebase.addValueEventListener(new com.firebase.client.ValueEventListener() {
                         @Override
@@ -92,18 +143,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
-            }
-        });
-
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(MainActivity.this,DetailsActivity.class);
-                intent.putExtra("location",arrayList.get(i).getOwnerId());
-                intent.putExtra("Condition",arrayList.get(i).getFlatCondition());
-                intent.putExtra("Available",arrayList.get(i).getAvailableFor());
-                startActivity(intent);
             }
         });
     }
