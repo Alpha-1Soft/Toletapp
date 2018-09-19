80ee6092f076f5edb87c6447484675a38f5bea94
hatanvir <tanvirhasan552@gmail.com>
2018-09-20 03:03:50 +0600

    FlatDetails model class updated

--- app/src/main/java/com/example/tanvir/to_letapp/models/FlatDetails.java
+++ app/src/main/java/com/example/tanvir/to_letapp/models/FlatDetails.java
@@ -3,15 +3,50 @@ package com.example.tanvir.to_letapp.models;
 public class FlatDetails {
     private String ownerId;
     private String flatLocation;
-    private String flatCondition;
-    private String availableFor;
+    private String bedroom;
+    private String kitchen;
+    private String bathroom;
+    private String rentDate;
+    private String totalRent;
+    private String condition;
 
 
-    public FlatDetails(String ownerId, String flatLocation, String flatCondition, String availableFor) {
+
+
+    public FlatDetails(String ownerId, String flatLocation, String bedroom, String kitchen, String bathroom, String rentDate, String condition,String totalRent) {
+
         this.ownerId = ownerId;
         this.flatLocation = flatLocation;
-        this.flatCondition = flatCondition;
-        this.availableFor = availableFor;
+        this.bedroom = bedroom;
+        this.kitchen = kitchen;
+        this.bathroom = bathroom;
+        this.rentDate = rentDate;
+        this.condition = condition;
+        this.totalRent=totalRent;
+    }
+
+    public String getTotalRent() {
+        return totalRent;
+    }
+
+    public String getBedroom() {
+        return bedroom;
+    }
+
+    public String getKitchen() {
+        return kitchen;
+    }
+
+    public String getBathroom() {
+        return bathroom;
+    }
+
+    public String getRentDate() {
+        return rentDate;
+    }
+
+    public String getCondition() {
+        return condition;
     }
 
     public String getOwnerId() {
@@ -22,11 +57,4 @@ public class FlatDetails {
         return flatLocation;
     }
 
-    public String getFlatCondition() {
-        return flatCondition;
-    }
-
-    public String getAvailableFor() {
-        return availableFor;
-    }
 }
