e0a615ad6a20dd889c5d0f181b92715fb17f1bcb
omarf <omar.faruq1998v@gmail.com>
2018-09-21 01:21:44 +0600

    add owner update acticity

---
+++ app/src/main/java/com/example/tanvir/to_letapp/activity/ownerActivity/OwnerUpdateActivity.java
@@ -0,0 +1,16 @@
+package com.example.tanvir.to_letapp.activity.ownerActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.tanvir.to_letapp.R;
+
+public class OwnerUpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_update);
+
+    }
+}
