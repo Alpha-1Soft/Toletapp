7bc1b69552ca655a08648acc706a7acfc74a4646
omarf <omar.faruq1998v@gmail.com>
2018-09-20 14:27:53 +0600

    Renter Update Activity update

---
+++ app/src/main/java/com/example/tanvir/to_letapp/activity/renterActivity/RenterUpdateActivity.java
@@ -0,0 +1,24 @@
+package com.example.tanvir.to_letapp.activity.renterActivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.tanvir.to_letapp.R;
+
+public class RenterUpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_renter_update);
+    }
+
+    public void renterSave(View view) {
+
+    }
+
+    public void rentercancel(View view) {
+    }
+}
