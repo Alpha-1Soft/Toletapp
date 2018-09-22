ab10137063c46befd76a25f71eb8eca64f689076
hatanvir <tanvirhasan552@gmail.com>
2018-09-22 22:37:34 +0600

    Login activity updated

--- app/src/main/java/com/example/tanvir/to_letapp/activity/DefaultLoginActivity.java
+++ app/src/main/java/com/example/tanvir/to_letapp/activity/DefaultLoginActivity.java
@@ -23,56 +23,67 @@ import com.google.firebase.database.ValueEventListener;
 
 public class DefaultLoginActivity extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
-    EditText rentalEmailOnLoginEt,rentalPassOnLoginEt;
+    EditText renterEmailOnLoginEt,renterPassOnLoginEt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_default_login);
 
-        rentalEmailOnLoginEt = findViewById(R.id.renterEmailOnLoginEt);
-        rentalPassOnLoginEt = findViewById(R.id.renterPassOnLoginEt);
+
+        renterEmailOnLoginEt = findViewById(R.id.renterEmailOnLoginEt);
+        renterPassOnLoginEt = findViewById(R.id.renterPassOnLoginEt);
+
         firebaseAuth = FirebaseAuth.getInstance();
 
     }
     DatabaseReference database = FirebaseDatabase.getInstance().getReference();//database reference
     public void login(View view) {
-        firebaseAuth.signInWithEmailAndPassword(rentalEmailOnLoginEt.getText().toString(),rentalPassOnLoginEt.getText().toString())
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            database.child("Owner").child("User").child(task.getResult().getUser().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
-                                @Override
-                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                    //checking current user id exist or not
-                                    //if exist then owner activity will be start
-                                    if (dataSnapshot.exists()) {
-                                        Toast.makeText(DefaultLoginActivity.this, "Login successfull as a owner", Toast.LENGTH_SHORT).show();
-                                        Intent intent = new Intent(DefaultLoginActivity.this, OwnerMainActivity.class);
-                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                        finish();
-                                        startActivity(intent);
-                                    } else {//else renter activity will be start
-                                        //finish();
-                                        Toast.makeText(DefaultLoginActivity.this, "Login successfull as a renter", Toast.LENGTH_SHORT).show();
-                                        Intent intent = new Intent(DefaultLoginActivity.this, RenterMainActivity.class);
-                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                        finish();
-                                        startActivity(intent);
+        if (renterEmailOnLoginEt.getText().toString().length() == 0 && renterPassOnLoginEt.getText().toString().length() == 0) {
+            Toast.makeText(DefaultLoginActivity.this, "Enter email and password", Toast.LENGTH_SHORT).show();
+        } else if (renterEmailOnLoginEt.getText().toString().length() == 0) {
+            Toast.makeText(DefaultLoginActivity.this, "Enter your email", Toast.LENGTH_SHORT).show();
+        } else if (renterPassOnLoginEt.getText().toString().length() < 6 || renterPassOnLoginEt.getText().toString().length() == 0) {
+            Toast.makeText(DefaultLoginActivity.this, "Enter at least 6 characters password", Toast.LENGTH_SHORT).show();
+        } else {
+            firebaseAuth.signInWithEmailAndPassword(renterEmailOnLoginEt.getText().toString(), renterPassOnLoginEt.getText().toString())
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                database.child("Owner").child("User").child(task.getResult().getUser().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                                        //checking current user id exist or not
+                                        //if exist then owner activity will be start
+                                        if (dataSnapshot.exists()) {
+                                            Toast.makeText(DefaultLoginActivity.this, "Login successful as a owner", Toast.LENGTH_SHORT).show();
+                                            Intent intent = new Intent(DefaultLoginActivity.this, OwnerMainActivity.class);
+                                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                            finish();
+                                            startActivity(intent);
+                                        } else {//else renter activity will be start
+                                            //finish();
+                                            Toast.makeText(DefaultLoginActivity.this, "Login successful as a renter", Toast.LENGTH_SHORT).show();
+                                            Intent intent = new Intent(DefaultLoginActivity.this, RenterMainActivity.class);
+                                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                            finish();
+                                            startActivity(intent);
+                                        }
                                     }
-                                }
 
-                                @Override
-                                public void onCancelled(@NonNull DatabaseError databaseError) {
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
 
-                                }
-                            });
+                                    }
+                                });
+                            } else {
+                                Toast.makeText(DefaultLoginActivity.this, "Login unsuccessful check your email and password", Toast.LENGTH_SHORT).show();
                             }
-                        else{
-                            Toast.makeText(DefaultLoginActivity.this, "Unsuccessful", Toast.LENGTH_SHORT).show();
                         }
-                    }
-                });
+                    });
+
+        }
     }
 
     public void NewAccount(View view) {
