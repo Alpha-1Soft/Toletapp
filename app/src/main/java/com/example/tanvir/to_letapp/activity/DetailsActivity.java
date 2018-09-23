b7f3c0785ec410e984ae5818092392c5c78883f5
hatanvir <tanvirhasan552@gmail.com>
2018-09-23 23:16:10 +0600

    Got ownerId and OwnerPostId from mainActivity successfully

--- app/src/main/java/com/example/tanvir/to_letapp/activity/DetailsActivity.java
+++ app/src/main/java/com/example/tanvir/to_letapp/activity/DetailsActivity.java
@@ -2,6 +2,7 @@ package com.example.tanvir.to_letapp.activity;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,16 +12,22 @@ import android.widget.Toast;
 import com.example.tanvir.to_letapp.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
 public class DetailsActivity extends AppCompatActivity {
-    TextView lonTv,avTv,conTv;
-    private FirebaseAuth firebaseAuth;
+    TextView locationTv,bedroomsTv, conditionTv,bathroomTv,rentForTv,rentAmountTv,rentDateTv;
+    String ownerId,ownerPostId;
+    FirebaseDatabase database;
+    DatabaseReference databaseReference;
     FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,49 +36,49 @@ public class DetailsActivity extends AppCompatActivity {
         viewIntialization();
 
         Intent intent = getIntent();
-        lonTv.setText(intent.getStringExtra("location"));
-        conTv.setText(intent.getStringExtra("Condition"));
-        avTv.setText(intent.getStringExtra("Available"));
+        ownerId = intent.getStringExtra("ownerId");
+        ownerPostId = intent.getStringExtra("ownerPostId");
     }
 
-    private void viewIntialization() {
-       lonTv = findViewById(R.id.locTv);
-       avTv = findViewById(R.id.avTv);
-       conTv = findViewById(R.id.conTv);
+    public void dataRetrieveFromDb(){
+
     }
 
     public void Request(View view) {
-        //firebaseAuth = FirebaseAuth.getInstance();
-            //mAuthListener = new FirebaseAuth.AuthStateListener() {
-           // @Override
-           // public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                String id = getIntent().getStringExtra("location");
-                FirebaseDatabase database;
-                DatabaseReference databaseReferenceOwner,databaseReferenceRenter;
-try {
 
-    String currentuser = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        //FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        final String id = getIntent().getStringExtra("location");
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference databaseReferenceOwner, databaseReferenceRenter;
 
-    database = FirebaseDatabase.getInstance();
-    databaseReferenceOwner = database.getReference().child("Owner").child("User").child(id).child("Request");
+        try {
+            String currentuser = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
-    //DataSnapshot dataSnapshot;
-    if (currentuser != null) {
-        databaseReferenceOwner.push().setValue("Request success");
-        Toast.makeText(DetailsActivity.this, "Request successful" + currentuser, Toast.LENGTH_SHORT).show();
-    } else {
-        Toast.makeText(DetailsActivity.this, "Request unsuccessful", Toast.LENGTH_SHORT).show();
+            database = FirebaseDatabase.getInstance();
+            databaseReferenceOwner = database.getReference().child("Owner").child("User").child(id).child("Request");
+
+            //DataSnapshot dataSnapshot;
+            if (currentuser != null) {
+                databaseReferenceOwner.push().setValue("Request success");
+                Toast.makeText(DetailsActivity.this, "Request successful" + currentuser, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(DetailsActivity.this, "Request unsuccessful", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        catch(Exception e){
+            Intent intent=new Intent(DetailsActivity.this,DefaultLoginActivity.class);
+            startActivity(intent);
+
+        }
     }
-}
-
-    catch(Exception e){
-       Intent intent=new Intent(DetailsActivity.this,DefaultLoginActivity.class);
-       startActivity(intent);
-
-    }
-
-            //}
-       // };
+    private void viewIntialization() {
+        locationTv = findViewById(R.id.locationTv);
+        bedroomsTv = findViewById(R.id.bedroomsTv);
+        conditionTv =findViewById(R.id.conditionTv);
+        bathroomTv = findViewById(R.id.bathroomEt);
+        rentForTv = findViewById(R.id.rentForTv);
+        rentAmountTv = findViewById(R.id.rentAmountTv);
+        rentDateTv = findViewById(R.id.rentDateTv);
     }
 }
