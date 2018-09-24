2f07ab6ac351822b166eca9e228a28467e5c8023
omarf <omar.faruq1998v@gmail.com>
2018-09-25 01:33:58 +0600

    OwnerUpdateActivity updated

--- app/src/main/java/com/example/tanvir/to_letapp/activity/ownerActivity/OwnerUpdateActivity.java
+++ app/src/main/java/com/example/tanvir/to_letapp/activity/ownerActivity/OwnerUpdateActivity.java
@@ -8,6 +8,8 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -24,42 +26,42 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class OwnerUpdateActivity extends AppCompatActivity {
+public class OwnerUpdateActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    EditText ownerNameEt,ownerEmailEt,ownerPhoneNumberEt,ownerAddresEt,ownerAgeEt;
-    TextView ownerGenderTv;
+    EditText ownerNameEt,ownerPhoneNumberEt,ownerAddresEt,ownerAgeEt;
     Spinner ownerRelagionSp;
-    String name,email,phoneNumber,address,age,gender,relagion;
+    String name,phoneNumber,address,age,relagion,relagionText;
     OwnerProfileFragment ownerProfileFragment;
+
+    String[] ownerRela={"Islam","Hinduism","Buddhists","Christians","None"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner_update);
         ownerNameEt=findViewById(R.id.ownerNameUp);
-        ownerEmailEt=findViewById(R.id.ownerEmailUp);
         ownerPhoneNumberEt=findViewById(R.id.ownerPhoneNumberUp);
         ownerAddresEt=findViewById(R.id.ownerAddressUp);
         ownerAgeEt=findViewById(R.id.ownerAgeUp);
         ownerRelagionSp=findViewById(R.id.ownerRelationUp);
-        ownerGenderTv=findViewById(R.id.ownerGenderUp);
 
         Firebase.setAndroidContext(this);
+        ownerRelagionSp.setOnItemSelectedListener(this);
+        ArrayAdapter<String> ownerRelaAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_dropdown_item_1line, ownerRela);
+        ownerRelaAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ownerRelagionSp.setAdapter(ownerRelaAdapter);
 
         name= getIntent().getStringExtra("Name");
-        email=getIntent().getStringExtra("Email");
         phoneNumber=getIntent().getStringExtra("Phone Number");
         address=getIntent().getStringExtra("Address");
         age=getIntent().getStringExtra("Age");
         relagion=getIntent().getStringExtra("Relagion");
-        gender=getIntent().getStringExtra("Gender");
 
        ownerNameEt.setText(name);
-       ownerEmailEt.setText(email);
        ownerPhoneNumberEt.setText(phoneNumber);
        ownerAddresEt.setText(address);
        ownerAgeEt.setText(age);
-       ownerGenderTv.setText(gender);
 
     }
 
@@ -86,10 +88,10 @@ public class OwnerUpdateActivity extends AppCompatActivity {
                     Toast.makeText(OwnerUpdateActivity.this, ""+dataSnapshot.getKey(), Toast.LENGTH_SHORT).show();
                     DatabaseReference databaseReference = databaseReferenceOwner.child(dataSnapshot.getKey());
                     databaseReference.child("Name").setValue(ownerNameEt.getText().toString());
-                    databaseReference.child("Email").setValue(ownerEmailEt.getText().toString());
                     databaseReference.child("Phone Number").setValue(ownerPhoneNumberEt.getText().toString());
                     databaseReference.child("Address").setValue(ownerAddresEt.getText().toString());
                     databaseReference.child("Age").setValue(ownerAgeEt.getText().toString());
+                    databaseReference.child("Relagion").setValue(relagionText);
 
                     setFragment(ownerProfileFragment);
 
@@ -130,5 +132,16 @@ public class OwnerUpdateActivity extends AppCompatActivity {
        // getFragmentManager().beginTransaction().detach(this).attach(this).commit();
         //fragmentTransaction.replace(R.id.frame_container, fragment);
         fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+        relagionText = adapterView.getItemAtPosition(i).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
