ddcc1c89a98588e8192f1a4d62c482f574513459
hatanvir <tanvirhasan552@gmail.com>
2018-09-26 15:13:20 +0600

    New constructor added

--- app/src/main/java/com/example/tanvir/to_letapp/models/OwnerRequest.java
+++ app/src/main/java/com/example/tanvir/to_letapp/models/OwnerRequest.java
@@ -1,37 +1,98 @@
 package com.example.tanvir.to_letapp.models;
 
 public class OwnerRequest {
-    private String name;
-    private String age;
-    private String profession;
+    private  String renterImage;
+    private  String renterName;
+    private  String renterAge;
+    private  String renterProfession;
+    private String renterPhoneNum;
+    private String renterAddress;
+    private String renterMonthlyIn;
+    private String renterMaritStatus;
+    private String renterGender;
+    private String renterReligion;
+    private String renterNationality;
     private String renterId;
-    private  String currentUser;
+    private String currentuser;
+    private String renterEmail;
 
-    public OwnerRequest(String name, String age, String profession,String renterId,String currentUser) {
-        this.name = name;
-        this.age = age;
-        this.profession = profession;
+    /*public OwnerRequest(String renterImage, String renterName, String renterAge, String renterProfession, String renterPhoneNum,
+                        String renterAddress, String renterMonthlyIn, String renterMaritStatus, String renterGender,
+                        String renterReligion, String renterNationality, String renterId, String currentuser) {
+    }*/
+
+    public OwnerRequest(String renterImage, String renterName, String renterAge, String renterProfession,
+                        String renterPhoneNum, String renterAddress, String renterMonthlyIn, String renterMaritStatus,
+                        String renterGender, String renterReligion, String renterNationality, String renterId, String currentuser,String renterEmail) {
+        this.renterImage = renterImage;
+        this.renterName = renterName;
+        this.renterAge = renterAge;
+        this.renterProfession = renterProfession;
+        this.renterPhoneNum = renterPhoneNum;
+        this.renterAddress = renterAddress;
+        this.renterMonthlyIn = renterMonthlyIn;
+        this.renterMaritStatus = renterMaritStatus;
+        this.renterGender = renterGender;
+        this.renterReligion = renterReligion;
+        this.renterNationality = renterNationality;
         this.renterId = renterId;
-        this.currentUser = currentUser;
+        this.currentuser = currentuser;
+        this.renterEmail = renterEmail;
     }
 
-    public String getCurrentUser() {
-        return currentUser;
+    public String getRenterEmail() {
+        return renterEmail;
+    }
+
+    public String getRenterImage() {
+        return renterImage;
+    }
+
+    public String getRenterName() {
+        return renterName;
+    }
+
+    public String getRenterAge() {
+        return renterAge;
+    }
+
+    public String getRenterProfession() {
+        return renterProfession;
+    }
+
+    public String getRenterPhoneNum() {
+        return renterPhoneNum;
+    }
+
+    public String getRenterAddress() {
+        return renterAddress;
+    }
+
+    public String getRenterMonthlyIn() {
+        return renterMonthlyIn;
+    }
+
+    public String getRenterMaritStatus() {
+        return renterMaritStatus;
+    }
+
+    public String getRenterGender() {
+        return renterGender;
+    }
+
+    public String getRenterReligion() {
+        return renterReligion;
+    }
+
+    public String getRenterNationality() {
+        return renterNationality;
     }
 
     public String getRenterId() {
         return renterId;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getAge() {
-        return age;
-    }
-
-    public String getProfession() {
-        return profession;
+    public String getCurrentuser() {
+        return currentuser;
     }
 }
